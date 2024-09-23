70a3d55853da8771389478a97e4fad3302f3d676
Sam Partee <Partees21@gmail.com>
2024-09-23 00:15:53 -0700

    New code styles and fonts

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -5,6 +5,14 @@ export function Logo() {
     return (
         <div className={styles.logo}>
             <img
+                className={"dark:hidden"}
+                loading="lazy"
+                src={"/images/logo/arcade-ai-logo.png"}
+                alt="Arcade AI Logo"
+            />
+            <img
+                className={"hidden dark:block"}
+                loading="lazy"
                 src={"/images/logo/arcadeai-title-dark.png"}
                 alt="Arcade AI Logo"
             />
