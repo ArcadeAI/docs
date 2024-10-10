ddeba02ab7f4f91c2b3aedc4b3215c8cc8d10b25
Nate Barbettini <nate@arcade-ai.com>
2024-10-10 09:07:02 -0700

    Add auth provider docs: Microsoft

---
+++ examples/code/integrations/microsoft/custom_auth.py
@@ -0,0 +1,23 @@
+from arcade.client.errors import APITimeoutError
+from arcade.client import Arcade, AuthProvider
+
+client = Arcade()
+
+user_id = "user@example.com"
+
+# Start the authorization process
+auth_response = client.auth.authorize(
+    provider=AuthProvider.microsoft,
+    scopes=["User.Read", "Files.Read"],
+    user_id=user_id,
+)
+
+if auth_response.status != "completed":
+    print("Please complete the authorization challenge in your browser:")
+    print(auth_response.auth_url)
+
+while auth_response.status != "completed":
+    try:
+        auth_response = client.auth.status(auth_response, wait=60)
+    except APITimeoutError:
+        continue
