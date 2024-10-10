2b60120bec441240aaa359958da3aa4652da2e23
Nate Barbettini <nate@arcade-ai.com>
2024-10-10 09:42:12 -0700

    Add auth provider docs: Zoom

---
+++ examples/code/integrations/zoom/custom_auth.py
@@ -0,0 +1,26 @@
+from arcade.client.errors import APITimeoutError
+from arcade.client import Arcade, AuthProvider
+
+client = Arcade()
+
+user_id = "user@example.com"
+
+# Start the authorization process
+auth_response = client.auth.authorize(
+    provider=AuthProvider.zoom,
+    scopes=["meeting:read:list_upcoming_meetings"],
+    user_id=user_id,
+)
+
+if auth_response.status != "completed":
+    print("Please complete the authorization challenge in your browser:")
+    print(auth_response.auth_url)
+
+while auth_response.status != "completed":
+    try:
+        auth_response = client.auth.status(auth_response, wait=60)
+    except APITimeoutError:
+        continue
+
+token = auth_response.context.token
+# Do something interesting with the token...
