693ab5dd73a24fe2042fa0a3aa6a5cdc124dbddb
Nate Barbettini <nate@arcade-ai.com>
2024-10-16 16:10:03 -0700

    WIP: Compress mega TOC (#45)
    * More LLM Additions (#42)
    
    * More llm docs
    
    * Update groq.mdx
    
    * Update groq.mdx
    
    * WIP: Compress mega TOC
    
    * WIP 2
    
    * Quick start -> quickstart
    
    * Update 'Tools > Build Tools' section
    
    * Update 'Tools > Evaluate Tools'
    
    * Update 'Agent Frameworks' and 'Tools > Serve Tools'
    
    * Polish Overview, Quickstart, Call tools with LLMs
    
    * Fix TOC nav in use-tools
    
    * Tiny quickstart fixes
    
    * Prose cleanup on Use tools > Overview, Call tools with LLMs
    
    * Update 'Self-hosting' and remove copied integrations files that were in /home
    
    * Remove old 'guides' pages
    
    * Add missing top level file descriptions
    
    * Update Auth section
    
    * Fix some links
    
    * Use tools > Use specific tools
    
    * Update more links
    
    * Rename two files to have hyphens
    
    * Use tools > Tool choice
    
    * Hide in progress pages
    
    * Last update
    
    ---------
    
    Co-authored-by: Sterling Dreyer <sterling@arcade-ai.com>
    Co-authored-by: Eric Gustin <eric@arcade-ai.com>

---
+++ examples/code/home/use-tools/call-tools-with-llms/github_stars_chatbot.py
@@ -0,0 +1,33 @@
+from arcade.core.config import config
+from openai import OpenAI
+
+# Initialize the OpenAI client, pointing to Arcade AI
+client = OpenAI(
+    base_url="https://api.arcade-ai.com/v1",
+    api_key=config.api_key,
+)
+
+# Get a unique identifier for your end user
+user_id = "you@example.com"
+
+while True:
+    # Ask the user for input
+    prompt = input("Enter your prompt (type 'exit' to quit): ")
+    if prompt.lower() == "exit":
+        break
+
+    # Use a tool and generate a response
+    response = client.chat.completions.create(
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": prompt},
+        ],
+        model="gpt-4o",
+        user=user_id,
+        tools=[
+            "GitHub.SetStarred",
+        ],
+        tool_choice="generate",
+    )
+
+    print(response.choices[0].message.content)
