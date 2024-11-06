ca3225e052ca53259fe5f09b5c97a141bbc92b2e
Eric Gustin <eric@arcade-ai.com>
2024-11-06 10:45:12 -0800

    Address comments

--- examples/code/home/use-tools/get-formatted-tool-definitions/get_openai_formatted_all_tools.py
+++ examples/code/home/use-tools/get-formatted-tool-definitions/get_openai_formatted_all_tools.py
@@ -2,11 +2,9 @@ from arcadepy import Arcade
 
 client = Arcade()
 
-# Get an iterator of pages of tools
-pages_iterator = client.tools.formatted.list(format="openai").iter_pages()
 
-# Flatten the pages into a list of all tools
-all_tools = [tool for page in pages_iterator for tool in page.items]
+# Get all tools formatted for OpenAI
+all_tools = list(client.tools.formatted.list(format="openai"))
 
 # Print the number of tools
 print(len(all_tools))
