e73b7835eb24ef63fb23ca092f3c365fc71dde15
Eric Gustin <34000337+EricGustin@users.noreply.github.com>
2024-11-07 09:51:17 -0800

    Show how to get/list formatted tools (#77)
    * Add get tool defs
    
    * Address comments

---
+++ examples/code/home/use-tools/get-formatted-tool-definitions/get_openai_formatted_all_tools.py
@@ -0,0 +1,10 @@
+from arcadepy import Arcade
+
+client = Arcade()
+
+
+# Get all tools formatted for OpenAI
+all_tools = list(client.tools.formatted.list(format="openai"))
+
+# Print the number of tools
+print(len(all_tools))
