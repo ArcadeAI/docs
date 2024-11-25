a0bb717808e1a5eead87fe5a23f36df509f3f8a4
Eric Gustin <34000337+EricGustin@users.noreply.github.com>
2024-11-24 22:31:42 -0800

    Add Code Sandbox Toolkit and new Gmail Tools (#84)

---
+++ public/examples/integrations/toolkits/code-sandbox/create_static_matplotlib_chart_example_llm_oai.py
@@ -0,0 +1,21 @@
+import os
+from openai import OpenAI
+
+USER_ID = "you@example.com"
+PROMPT = "Create a static matplotlib pie chart for this data: Two green apples, 5 red apples, 3 yellow apples."
+TOOL_NAME = "CodeSandbox.CreateStaticMatplotlibChart"
+
+client = OpenAI(
+    base_url="https://api.arcade-ai.com", api_key=os.environ.get("ARCADE_API_KEY")
+)
+
+response = client.chat.completions.create(
+    messages=[
+        {"role": "user", "content": PROMPT},
+    ],
+    model="gpt-4o-mini",
+    user=USER_ID,
+    tools=[TOOL_NAME],
+    tool_choice="execute",
+)
+print(response)  # The returned base64 encoded image needs to be decoded now
---
+++ public/examples/integrations/toolkits/google/gmail/get_thread_example_call_tool.py
@@ -0,0 +1,26 @@
+from arcadepy import Arcade
+
+client = Arcade()  # Automatically finds the `ARCADE_API_KEY` env variable
+
+USER_ID = "you@example.com"
+TOOL_NAME = "Google.GetThread"
+
+auth_response = client.tools.authorize(
+    tool_name=TOOL_NAME,
+    user_id=USER_ID,
+)
+
+if auth_response.status != "completed":
+    print(f"Click this link to authorize: {auth_response.authorization_url}")
+
+# Wait for the authorization to complete
+client.auth.wait_for_completion(auth_response)
+
+inputs = {"thread_id": "your-thread-id"}
+
+response = client.tools.execute(
+    tool_name=TOOL_NAME,
+    inputs=inputs,
+    user_id=USER_ID,
+)
+print(response)
