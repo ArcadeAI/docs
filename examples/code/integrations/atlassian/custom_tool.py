11ca4e14c5c0becdb62360c852a71cf66ca97407
Wils Dawson <wils@arcade-ai.com>
2024-11-27 11:42:19 -0800

    Add Atlassian as a supported Auth Provider.

---
+++ examples/code/integrations/atlassian/custom_tool.py
@@ -0,0 +1,28 @@
+from typing import Annotated, Optional
+
+import httpx
+
+from arcade.sdk import ToolContext, tool
+from arcade.sdk.auth import Atlassian
+
+
+@tool(
+    requires_auth=Atlassian(
+        scopes=["read:jira-work"],
+    )
+)
+async def list_projects(
+    context: ToolContext,
+    query: Annotated[
+        Optional[str],
+        "The query to filter the projects by. Defaults to empty string to list all projects.",
+    ] = "",
+) -> Annotated[dict, "The list of projects in a user's Jira instance"]:
+    """List a Jira user's projects."""
+    url = f"https://api.atlassian.com/ex/jira/<cloudId>/rest/api/3/project/search?query={query}"
+    headers = {"Authorization": f"Bearer {context.authorization.token}"}
+
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url, headers=headers)
+        response.raise_for_status()
+        return response.json()
