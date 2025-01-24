cf6ca11d8753945754d6a4614fdca3afba15254d
Renato Byrro <rmbyrro@gmail.com>
2025-01-23 18:32:02 -0800

    add example for linkedin custom auth

--- examples/code/integrations/linkedin/custom_auth.py
+++ examples/code/integrations/linkedin/custom_auth.py
@@ -1,4 +1,7 @@
+import requests
+
 from arcadepy import Arcade
+
 
 client = Arcade()  # Automatically finds the `ARCADE_API_KEY` env variable
 
@@ -18,5 +21,42 @@ if auth_response.status != "completed":
 # Wait for the authorization to complete
 auth_response = client.auth.wait_for_completion(auth_response)
 
+if not auth_response.context.token:
+    raise ValueError("No token found in auth response")
+
 token = auth_response.context.token
-# TODO: Do something interesting with the token...
+
+# We will not post a message to LinkedIn in this example
+
+# The user_id is needed by the LinkedIn API
+user_id = (
+    None
+    if not auth_response.context.authorization
+    else auth_response.context.authorization.user_info.get("sub")
+)
+
+if not user_id:
+    raise ValueError("User ID not found.")
+
+# Prepare the payload data for the LinkedIn API
+message = "Hello, from Arcade.dev!"
+payload = {
+    "author": f"urn:li:person:{user_id}",
+    "lifecycleState": "PUBLISHED",
+    "specificContent": {
+        "com.linkedin.ugc.ShareContent": {
+            "shareCommentary": {"text": message},
+            "shareMediaCategory": "NONE",
+        }
+    },
+    "visibility": {"com.linkedin.ugc.MemberNetworkVisibility": "PUBLIC"},
+}
+
+# Send the request to the LinkedIn API
+response = requests.post(
+    "https://api.linkedin.com/v2/ugcPosts",
+    headers={"Authorization": f"Bearer {token}"},
+    json=payload,
+)
+response.raise_for_status()
+print(response.json())
