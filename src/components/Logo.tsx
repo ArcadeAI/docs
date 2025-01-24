b669139b48aeb6fb05ad3870c292a488ad6d3105
Sergio Serrano <35855882+sdserranog@users.noreply.github.com>
2025-01-24 15:16:08 -0400

    Center content in landing page and fix logo in mobile (#121)

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,22 +1,34 @@
 export function Logo() {
   return (
-    <div className="mr-16 flex w-40 items-center">
-      <img
-        className="dark:hidden"
-        loading="lazy"
-        src={"/images/logo/arcade-ai-logo.png"}
-        alt="Arcade AI Logo"
-        width={150}
-        height={30}
-      />
-      <img
-        className="hidden dark:block"
-        loading="lazy"
-        src={"/images/logo/arcadeai-title-dark.png"}
-        alt="Arcade AI Logo"
-        width={150}
-        height={30}
-      />
-    </div>
+    <>
+      <div className="mr-16 hidden w-40 items-center md:block">
+        <img
+          className="dark:hidden"
+          loading="lazy"
+          src={"/images/logo/arcade-ai-logo.png"}
+          alt="Arcade AI Logo"
+          width={150}
+          height={30}
+        />
+        <img
+          className="hidden dark:block"
+          loading="lazy"
+          src={"/images/logo/arcadeai-title-dark.png"}
+          alt="Arcade AI Logo"
+          width={150}
+          height={30}
+        />
+      </div>
+      <div className="mr-16 block w-6 md:hidden">
+        <img
+          className="dark:block"
+          loading="lazy"
+          src={"/images/logo/arcadeai.png"}
+          alt="Arcade AI Logo"
+          width={30}
+          height={30}
+        />
+      </div>
+    </>
   );
 }
