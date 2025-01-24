48e25e44617f8901c017802460096faff74b480d
Wils Dawson <wils@arcade.dev>
2025-01-24 15:51:33 -0800

    Address Eric's feedback. (#124)

--- pages/home/use-tools/_meta.ts
+++ pages/home/use-tools/_meta.ts
@@ -1,8 +1,8 @@
 export default {
   "tools-overview": "Introduction",
-  "tool-choice": "Tool Choice",
   "call-tools-directly": "Tool execution",
   "call-tools-with-models": "Tool Augmented Generation",
+  "tool-choice": "Tool Choice",
   "specify-tools": "Selecting tools",
   "get-tool-definitions": "Tool formats",
 };
