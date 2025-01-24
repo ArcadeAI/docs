3ea6808e8c3e4613b498c9f47a78e0685c7b84c8
Sergio Serrano <35855882+sdserranog@users.noreply.github.com>
2025-01-23 23:59:32 -0400

    Add some JS examples to our docs (#117)

--- examples/code/home/use-tools/get-formatted-tool-definitions/get_openai_formatted_tool.py
+++ examples/code/home/use-tools/get-formatted-tool-definitions/get_openai_formatted_tool.py
@@ -3,8 +3,6 @@ from arcadepy import Arcade
 client = Arcade()
 
 # Get a specific tool formatted for OpenAI
-github_star_repo = client.tools.formatted.get(
-    tool_id="Github.SetStarred", format="openai"
-)
+github_star_repo = client.tools.formatted.get(name="Github.SetStarred", format="openai")
 
 print(github_star_repo)
