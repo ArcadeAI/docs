0c729652870c2169178a8e32c8a46456fef1b032
Renato Byrro <rmbyrro@gmail.com>
2025-01-23 18:43:25 -0800

    add custom auth example with github

--- examples/code/integrations/github/custom_auth.py
+++ examples/code/integrations/github/custom_auth.py
@@ -1,3 +1,4 @@
+import requests
 from arcadepy import Arcade
 
 client = Arcade()  # Automatically finds the `ARCADE_API_KEY` env variable
@@ -17,5 +18,22 @@ if auth_response.status != "completed":
 # Wait for the authorization to complete
 auth_response = client.auth.wait_for_completion(auth_response)
 
+if not auth_response.context.token:
+    raise ValueError("No token found in auth response")
+
 token = auth_response.context.token
-# TODO: Do something interesting with the token...
+
+# We are going to count the stargazers of a GitHub repository
+owner = "ArcadeAI"
+name = "arcade-ai"
+headers = {
+    "Accept": "application/vnd.github+json",
+    "Authorization": f"Bearer {token}",
+    "X-GitHub-Api-Version": "2022-11-28",
+}
+url = f"https://api.github.com/repos/{owner}/{name}"
+
+response = requests.get(url, headers=headers)
+response.raise_for_status()
+
+print(response.json().get("stargazers_count"))
