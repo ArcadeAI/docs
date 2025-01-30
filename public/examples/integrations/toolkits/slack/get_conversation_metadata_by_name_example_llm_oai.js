1cce7073a020a82cd14dca5fc28cb192e44bb79e
Sergio Serrano <35855882+sdserranog@users.noreply.github.com>
2025-01-30 13:55:51 -0400

    Add JS examples to our slack toolkit (#144)
    * Add JS examples to our slack toolkit
    
    * Add examples to our slack page

---
+++ public/examples/integrations/toolkits/slack/get_conversation_metadata_by_name_example_llm_oai.js
@@ -0,0 +1,22 @@
+import OpenAI from 'openai';
+
+const USER_ID = 'you@example.com';
+const PROMPT = "Retrieve the metadata for the conversation named 'general'."
+const TOOL_NAME = 'Slack.GetConversationMetadataByName';
+
+const client = new OpenAI({
+    baseURL: 'https://api.arcade.dev',
+    apiKey: process.env.ARCADE_API_KEY
+});
+
+const response = await client.chat.completions.create({
+    messages: [
+        { role: 'user', content: PROMPT }
+    ],
+    model: 'gpt-4o-mini',
+    user: USER_ID,
+    tools: [TOOL_NAME],
+    tool_choice: 'generate'
+});
+
+console.log(response.choices[0].message.content); 
