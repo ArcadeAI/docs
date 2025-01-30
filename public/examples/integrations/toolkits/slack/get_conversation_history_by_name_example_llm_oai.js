1ccf933d969cbf1c6dd1adb1f7813528109681e9
Sergio Serrano <sdserranog@gmail.com>
2025-01-30 11:19:05 -0400

    Add JS examples to our slack toolkit

---
+++ public/examples/integrations/toolkits/slack/get_conversation_history_by_name_example_llm_oai.js
@@ -0,0 +1,22 @@
+import OpenAI from 'openai';
+
+const USER_ID = 'you@example.com';
+const PROMPT = "Fetch the conversation history for the conversation named 'general' between '2023-01-01 00:00:00' and '2023-01-31 23:59:59'."
+const TOOL_NAME = 'Slack.GetConversationHistoryByName';
+
+const client = new OpenAI({
+    baseURL: 'https://api.arcade.dev',
+    apiKey: process.env.ARCADE_API_KEY
+});
+
+const response = await client.chat.completions.create({
+    messages: [
+        { role: 'user', content: PROMPT }
+    ],
+    model: 'gpt-4o-mini',
+    user: USER_ID,
+    tools: [TOOL_NAME],
+    tool_choice: 'generate'
+});
+
+console.log(response.choices[0].message.content); 
