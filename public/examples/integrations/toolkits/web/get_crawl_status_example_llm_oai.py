37f072a54b5977c16b3023d234319c761fac20c6
Sergio Serrano <sdserranog@gmail.com>
2025-01-31 10:21:01 -0400

    Add JS examples to the web toolkit

--- public/examples/integrations/toolkits/web/get_crawl_status_example_llm_oai.py
+++ public/examples/integrations/toolkits/web/get_crawl_status_example_llm_oai.py
@@ -1,15 +1,21 @@
-from arcadepy import Arcade
+import os
+from openai import OpenAI
 
 USER_ID = "you@example.com"
+PROMPT = "Get the status of crawl with ID your_crawl_id."
 TOOL_NAME = "Web.GetCrawlStatus"
 
-client = Arcade()  # Automatically finds the `ARCADE_API_KEY` env variable
-
-tool_input = {"crawl_id": "your_crawl_id"}
-
-response = client.tools.execute(
-    tool_name=TOOL_NAME,
-    input=tool_input,
-    user_id=USER_ID,
+client = OpenAI(
+    base_url="https://api.arcade.dev", api_key=os.environ.get("ARCADE_API_KEY")
 )
-print(response)
+
+response = client.chat.completions.create(
+    messages=[
+        {"role": "user", "content": PROMPT},
+    ],
+    model="gpt-4o-mini",
+    user=USER_ID,
+    tools=[TOOL_NAME],
+    tool_choice="generate",
+)
+print(response.choices[0].message.content)
