2b483810db4838eccc59df3fcfe921e6b5196dbb
Wils Dawson <wils@arcade.dev>
2025-02-28 13:49:12 -0800

    Wils/posthog (#178)
    * Enable posthog in docs.
    
    * add chrome debug to launch.json

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,42 @@
 import { ThemeProvider } from "next-themes";
+import { useRouter } from "next/router";
 import Script from "next/script";
+import posthog from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
+import { useEffect } from "react";
 import "../styles/globals.css";
-import * as React from "react";
 
 function Docs({ Component, pageProps }) {
+  const router = useRouter();
+  useEffect(() => {
+    if (process.env.NEXT_PUBLIC_POSTHOG_KEY) {
+      posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+        api_host:
+          process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://us.i.posthog.com",
+        ui_host:
+          process.env.NEXT_PUBLIC_POSTHOG_UI_HOST || "https://us.posthog.com",
+        disable_session_recording: true,
+        loaded: (posthog) => {
+          if (process.env.NODE_ENV === "development") posthog.debug();
+        },
+      });
+
+      const handleRouteChangeComplete = () => posthog?.capture("$pageview");
+
+      const handleRouteChangeStart = () => posthog?.capture("$pageleave");
+
+      router.events.on("routeChangeComplete", handleRouteChangeComplete);
+      router.events.on("routeChangeStart", handleRouteChangeStart);
+
+      return () => {
+        router.events.off("routeChangeComplete", handleRouteChangeComplete);
+        router.events.off("routeChangeStart", handleRouteChangeStart);
+      };
+    } else {
+      console.warn("Analytics is disabled because no key is set");
+    }
+  }, [router.events]);
+
   return (
     <>
       {/* Google tag (gtag.js) */}
@@ -21,9 +54,11 @@ function Docs({ Component, pageProps }) {
       `}
       </Script>
 
-      <ThemeProvider attribute="class" defaultTheme="dark">
-        <Component {...pageProps} />
-      </ThemeProvider>
+      <PostHogProvider client={posthog}>
+        <ThemeProvider attribute="class" defaultTheme="dark">
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </PostHogProvider>
     </>
   );
 }
