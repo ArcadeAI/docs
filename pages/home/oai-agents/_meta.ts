8b68d1798fd142d38c0ea95c85d621db107eaecf
Sam Partee <Partees21@gmail.com>
2025-03-31 18:21:47 -0700

    all

--- pages/home/oai-agents/_meta.ts
+++ pages/home/oai-agents/_meta.ts
@@ -2,4 +2,5 @@ export default {
   overview: "Overview",
   "use-arcade-tools": "Using Arcade tools",
   "user-auth-interrupts": "Managing user authorization",
+  "creating-custom-tools": "Creating custom tools",
 };
