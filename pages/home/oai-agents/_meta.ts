e06022c9f5ce75ea2b0b3b19854f5f8f29d310ca
Sam Partee <Partees21@gmail.com>
2025-04-03 23:15:32 -0700

    auth flow

--- pages/home/oai-agents/_meta.ts
+++ pages/home/oai-agents/_meta.ts
@@ -2,5 +2,4 @@ export default {
   overview: "Overview",
   "use-arcade-tools": "Using Arcade tools",
   "user-auth-interrupts": "Managing user authorization",
-  "creating-custom-tools": "Creating custom tools",
 };
