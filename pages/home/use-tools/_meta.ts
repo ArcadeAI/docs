02f4ac029144b803ba2dc22a70b7d64c7e54d3e7
Evan Tahler <evan@arcade.dev>
2025-05-06 16:26:14 -0700

    Remove LLM/Model APIs

--- pages/home/use-tools/_meta.ts
+++ pages/home/use-tools/_meta.ts
@@ -1,7 +1,5 @@
 export default {
   "tools-overview": "Introduction",
-  "call-tools-with-models": "Call Tools with LLMs",
   "call-tools-directly": "Call Tools Directly",
-  "tool-choice": "Tool Choice",
   "get-tool-definitions": "Tool formats",
 };
