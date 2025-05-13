73bc2c36ae658f1c3fc30806a150d5e9ff8de517
Nate Barbettini <nate@arcade.dev>
2025-05-13 14:29:59 -0700

    Add VSCode MCP documentation (#275)
    * Add VSCode MCP documentation
    
    * Add coming soon info

---
+++ pages/home/mcp-desktop-clients/_meta.ts
@@ -0,0 +1,5 @@
+export default {
+  "claude-desktop-client": "Use Arcade with Claude Desktop",
+  "vscode-client": "Use Arcade in Visual Studio Code",
+  //"remote-code-client": "Arcade as an MCP Server",
+};
--- pages/home/mcp/_meta.ts
+++
@@ -1,4 +0,0 @@
-export default {
-  "claude-desktop-client": "Arcade with Claude Desktop",
-  //"remote-code-client": "Arcade as an MCP Server",
-};
