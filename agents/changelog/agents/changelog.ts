cdd04809d1a2cd187faf2bac56d2c296e5f34be0
Evan Tahler <evan@arcade.dev>
2025-07-06 18:05:26 -0700

    Changelog Agent (#333)
    * wip - changelog agent
    
    * scaffolding
    
    * scaffolding
    
    * re-add
    
    * url scheme
    
    * readme
    
    * setup
    
    * cleanup
    
    * cleanup
    
    * load PR content
    
    * ignore agents in build
    
    * rename
    
    * install stuff
    
    * automated changelogs
    
    * remove internal notes

---
+++ agents/changelog/agents/changelog.ts
@@ -0,0 +1,61 @@
+import { WrappedAgent } from "../classes/wrappedAgent";
+import type { Config } from "../classes/config";
+import type { Logger } from "../classes/logger";
+import { getNewCommitsTool } from "../tools/getNewCommitsAndPRs";
+import { readFileTool } from "../tools/readFile";
+import { writeFileTool } from "../tools/writeFile";
+
+export class ChangelogAgent extends WrappedAgent {
+  constructor(config: Config, logger: Logger) {
+    const systemPrompt = `
+You are a helpful assistant that writes changelogs for the Arcade.dev software projects.
+
+Your goal is to load all the new git commits and pull requests from provided Github repositories since the last entry in the changelog.md file, and produce a list of the changes for our customers.  You will use the GitHub API to get the changes and pull requests for all of the relevant projects.
+
+There are 5 possible categories of changes:
+- Frameworks
+- Toolkits
+- CLI and TDK
+- Platform and Engine
+- Misc
+
+There are 4 possible types of changes, which each have an emoji associated with them:
+- 🚀 Feature
+- 🐛 Bugfix
+- 📝 Documentation
+- 🔧 Maintenance
+
+The steps to follow are:
+1. Load the changelog.mdx file and note the date of the most recent entry.
+2. Load all new commits since the most recent entry in the changelog.mdx file from the provided Github repositories.
+3. Categorize the changes into the 5 categories and 3 types.  If the change is not in one of the categories, it should be categorized as "Misc".  Ignore small changes that are not worth mentioning - use your judgement.
+4. Update the changelog.mdx file with the new changes.  The changelog should be in the same format as the changelog.mdx file.  Do not include any other text in the changelog.mdx file.
+
+Report the steps you took to update the changelog when complete, or any errors you encountered.
+`;
+
+    const tools = [
+      readFileTool(config, logger),
+      writeFileTool(config, logger),
+      getNewCommitsTool(config, logger),
+    ];
+
+    super("ChangelogAgent", systemPrompt, tools, [], config, logger);
+  }
+
+  async generate(changelogPath: string, repositories: string[]) {
+    this.logger.startSpan(
+      `Generating changelog from changes in ${repositories.join(", ")}...`,
+    );
+
+    const result = await this.run(
+      `
+      Today is ${new Date().toISOString().split("T")[0]}.
+      The full path to the changelog.md that you will be updating is \`${changelogPath}\`.
+      The Github repositories to load commits from are: ${repositories.join(", ")}
+      `,
+    );
+
+    this.logger.endSpan(result.finalOutput);
+  }
+}
