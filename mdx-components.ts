37b678f09328415e4bf2be745f6160ca09653a2a
Rachel Lee Nabors <nearestnabors@users.noreply.github.com>
2025-10-09 15:21:09 +0100

    adds overview

--- mdx-components.ts
+++ mdx-components.ts
@@ -2,6 +2,7 @@ import type { UseMDXComponents } from "nextra/mdx-components";
 import { useMDXComponents as getDocsMdxComponents } from "nextra-theme-docs";
 import CustomPre from "./app/_components/custom-pre";
 import { GlossaryTerm } from "./app/_components/glossary-term";
+import { GuideOverview } from "./app/_components/guide-overview";
 import Note from "./app/_components/note";
 
 const docsComponents = getDocsMdxComponents({
@@ -13,6 +14,7 @@ export const useMDXComponents: UseMDXComponents<typeof docsComponents> = <T>(
 ) => ({
   ...docsComponents,
   GlossaryTerm,
+  GuideOverview,
   Note,
   ...components,
 });
