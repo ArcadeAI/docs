498a491dfd7aee75f1dd37ac346191fdb3c53436
Evan Tahler <evan@arcade.dev>
2025-10-13 14:45:23 -0700

    cleanup

--- app/en/home/build-tools/_meta.tsx
+++ app/en/home/build-tools/_meta.tsx
@@ -3,6 +3,6 @@ export default {
   "tool-context": "Tools and Context",
   "create-a-tool-with-auth": "Create a tool with auth",
   "create-a-tool-with-secrets": "Create a tool with secrets",
-  "provide-good-tool-errors": "Provide good tool errors",
+  "providing-useful-tool-errors": "Providing useful tool errors",
   "retry-tools-with-improved-prompt": "Retry tools with improved prompt",
 };
