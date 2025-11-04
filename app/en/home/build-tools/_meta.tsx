2dc75129b7894a0e488d921383b0402819c7aacb
Eric Gustin <eric@arcade.dev>
2025-11-03 18:00:24 -0800

    Update 'Create MCP server' and 'create auth tool'

--- app/en/home/build-tools/_meta.tsx
+++ app/en/home/build-tools/_meta.tsx
@@ -1,8 +1,8 @@
 export default {
   "create-a-mcp-server": "Create an MCP Server",
-  "tool-context": "Tools and Context",
   "create-a-tool-with-auth": "Create a tool with auth",
   "create-a-tool-with-secrets": "Create a tool with secrets",
+  "tool-context": "Tools and Context",
   "logging-mcp-tool-activity": "Logging tool activity",
   "organize-mcp-server-tools": "Organize MCP server tools",
   "providing-useful-tool-errors": "Providing useful tool errors",
