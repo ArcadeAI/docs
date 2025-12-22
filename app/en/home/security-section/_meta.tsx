b7c01c8d767dd970728b80f719f8d3b251759e8e
Rachel Lee Nabors <nearestnabors@users.noreply.github.com>
2025-12-22 20:45:42 +0000

    fixing up secure-multi-user-auth

--- app/en/home/security-section/_meta.tsx
+++ app/en/home/security-section/_meta.tsx
@@ -1,5 +1,6 @@
 export default {
   "platform-security": "Platform security",
+  "users-verification": "Verify your users",
   "enterprise-sso": "Enterprise single sign on",
   "rbac-config": "Configure role-based access control for your organization",
 };
