f2dced31b275a8acb4b9264eb7f51a2d611bfae4
Rachel Lee Nabors <nearestnabors@users.noreply.github.com>
2025-12-29 13:59:49 +0000

    removing missing entry

--- app/en/home/sharing-with-end-users/_meta.tsx
+++ app/en/home/sharing-with-end-users/_meta.tsx
@@ -1,5 +1,4 @@
 export default {
-  "multi-user-auth": "Set up secure multi-user auth",
   "configure-oauth-provider": "Configure an OAuth provider",
   "brand-auth-flow": "Brand your Auth flow",
 };
