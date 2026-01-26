68de7cbcf2f0b5fad12b2fd20abd07dee505995b
jottakka <fjlimal@gmail.com>
2026-01-26 17:21:34 -0300

    custom sections woriking

---
+++ app/en/resources/integrations/social/_meta.tsx
@@ -0,0 +1,34 @@
+import type { MetaRecord } from "nextra";
+
+const meta: MetaRecord = {
+  "linkedin": {
+    title: "LinkedIn",
+    href: "/en/resources/integrations/preview/linkedin",
+  },
+  "microsoftteams": {
+    title: "Microsoft Teams",
+    href: "/en/resources/integrations/preview/microsoftteams",
+  },
+  "reddit": {
+    title: "Reddit",
+    href: "/en/resources/integrations/preview/reddit",
+  },
+  "slack": {
+    title: "Slack",
+    href: "/en/resources/integrations/preview/slack",
+  },
+  "slackapi": {
+    title: "Slack API",
+    href: "/en/resources/integrations/preview/slackapi",
+  },
+  "x": {
+    title: "X",
+    href: "/en/resources/integrations/preview/x",
+  },
+  "zoom": {
+    title: "Zoom",
+    href: "/en/resources/integrations/preview/zoom",
+  },
+};
+
+export default meta;
