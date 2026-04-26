455097a310692f7eef555702a0454b6c922d650f
jottakka <203343514+jottakka@users.noreply.github.com>
2026-04-26 11:23:01 +0000

    [AUTO] Adding MCP Servers docs update

--- app/en/resources/integrations/search/_meta.tsx
+++ app/en/resources/integrations/search/_meta.tsx
@@ -53,14 +53,6 @@ const meta: MetaRecord = {
     title: "Exa API",
     href: "/en/resources/integrations/search/exa-api",
   },
-  "-- Partner": {
-    type: "separator",
-    title: "Partner",
-  },
-  tavily: {
-    title: "Tavily",
-    href: "/en/resources/integrations/search/tavily",
-  },
 };
 
 export default meta;
