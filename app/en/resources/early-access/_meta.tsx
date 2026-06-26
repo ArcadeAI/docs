d77ecc61ab58b247416ac03a789f5fecc3563867
vfanelle <vfanelle@gmail.com>
2026-06-25 17:43:57 -0700

    Add resources/early-access/_meta.tsx with Warp Pipes and Arcade Registry links

---
+++ app/en/resources/early-access/_meta.tsx
@@ -0,0 +1,14 @@
+import type { MetaRecord } from "nextra";
+
+const meta: MetaRecord = {
+  "warp-pipes": {
+    title: "Warp Pipes",
+    href: "/en/guides/deployment-hosting/warp-pipes",
+  },
+  "registry": {
+    title: "Arcade Registry",
+    href: "/en/resources/registry-early-access",
+  },
+};
+
+export default meta;
